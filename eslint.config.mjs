bdc85c8abc2ced262ceafa579982258285aab57a
Case Walker <case@innovation.nj.gov>
2026-04-17 01:52:10 -0400

    chore: Upgrade NextJS, and React, and bump Cypress to support this

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,25 +1,16 @@
-import { dirname } from "node:path";
-import { fileURLToPath } from "node:url";
-import { FlatCompat } from "@eslint/eslintrc";
+import nextTypescript from "eslint-config-next/typescript";
+import nextCoreWebVitals from "eslint-config-next/core-web-vitals";
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = dirname(__filename)
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-})
-
-const eslintConfig = [
-  ...compat.extends('next/core-web-vitals', 'next/typescript'),
+export default [
+  ...nextTypescript,
+  ...nextCoreWebVitals,
   {
     ignores: [
-      'node_modules/**',
-      '.next/**',
-      'out/**',
-      'build/**',
-      'next-env.d.ts',
+      "node_modules/**",
+      ".next/**",
+      "out/**",
+      "build/**",
+      "next-env.d.ts",
     ],
   },
-]
-
-export default eslintConfig
+];
